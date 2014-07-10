6c4427fc74e6d3ef9d1fbb216ef3e6a3ef7f04fb
vyachs <Vyacheslav.Soldatkin@kodep.ru>
2014-07-10 16:13:22 +0400

    added methods country and country_code

--- lib/reverse_geo/reverse_geo.rb
+++ lib/reverse_geo/reverse_geo.rb
@@ -12,14 +12,24 @@ class ReverseGeo
     @countries = RGeo::GeoJSON.decode(File.open(file).read, :json_parser => :json, :geo_factory => @factory)
   end
 
+  def country_code(opts = {})
+    country_search('ISO_A3', opts)
+  end
+
   def country(opts = {})
+    country_search('Country', opts)
+  end
+
+  private
+
+  def country_search(property_name, opts)
     raise ArgumentError, "Did not supply a valid lat ([-90,90]) and lng ([-180,180]). You supplied lat: #{opts[:lat]}, lng: #{opts[:lng]}" unless opts[:lat] && opts[:lat].between?(-90, 90) && opts[:lng] && opts[:lng].between?(-180, 180)
 
     poi = @factory.point(opts[:lng], opts[:lat])
     @countries.each do |country|
-      return country.properties['ISO_A3'] if poi.within?(country.geometry)
+      return country.properties[property_name] if poi.within?(country.geometry)
     end
 
-    return nil#fallback if we didn't find anything
+    nil#fallback if we didn't find anything
   end
 end
